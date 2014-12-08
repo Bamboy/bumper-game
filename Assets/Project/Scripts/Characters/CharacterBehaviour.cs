21bcb1ef923ec89c3c35bd74510419002ee8849d
Stephan Ennen <bamboy360@gmail.com>
2014-12-08 13:14:14 -0800

    Added Progrids, created character base class and functions, basic player movement, basic pit functionality.

---
+++ Assets/Project/Scripts/Characters/CharacterBehaviour.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Our base class for all characters
+namespace Excelsion.Characters
+{
+	[RequireComponent(typeof(Rigidbody2D))]
+	public abstract class CharacterBehaviour : MonoBehaviour 
+	{
+		protected float zSpeed = 0.0f;
+		//protected virtual void Update()
+		//{
+		//	transform.Translate(0,0, zSpeed * Time.deltaTime);
+		//}
+		protected virtual void FixedUpdate () 
+		{
+			transform.Translate(0,0, zSpeed * Time.deltaTime);
+		}
+		
+		
+		//Called from a pit when a character enters it.
+		public virtual void OnPitEnter()
+		{
+			zSpeed = 10.0f;
+			//rigidbody2D.isKinematic = true;
+			Kill();
+		}
+		public virtual void Kill()
+		{
+			OnKilled();
+			Destroy(this.gameObject);
+		}
+		protected virtual void OnKilled()
+		{
+			//Do stuff you wanna do before dying.
+			return;
+		}
+
+		//Called when we hit a wall.
+		protected virtual void OnWallCollision( Collision2D col )
+		{
+			return;
+		}
+
+		protected virtual void OnCollisionEnter2D( Collision2D col )
+		{
+			CharacterBehaviour unit = col.gameObject.GetComponent< CharacterBehaviour >();
+			if( unit != null )
+			{
+				this.OnHitCharacter( unit );
+			}
+			else
+			{
+				//Assume we hit a wall.
+				this.OnWallCollision( col );
+			}
+		}
+	
+		//What we do when we make physical contact with another character
+		protected virtual void OnHitCharacter( CharacterBehaviour unit )
+		{
+			unit.OnCharacterCollision( this as CharacterBehaviour );
+		}
+
+		//Called from the other character when they hit us.
+		public virtual void OnCharacterCollision( CharacterBehaviour other )
+		{
+			return;
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
---
+++ Assets/Project/Scripts/Hazard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Excelsion.Characters;
+
+public class Hazard : MonoBehaviour 
+{
+
+	void OnTriggerEnter2D( Collider2D c )
+	{
+		CharacterBehaviour unit = c.gameObject.GetComponent< CharacterBehaviour >();
+		Debug.Log ("Entered pit!");
+		if( unit != null )
+		{
+			Debug.Log ("Entered pit and has unit!");
+			unit.OnPitEnter();
+		}
+	}
+
+}
